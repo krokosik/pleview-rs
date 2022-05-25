7bce847ba952756d05ecb7b6e4652fe19dd051fc
wkrasnicki <wikkra@gmail.com>
2022-05-25 23:39:24 +0200

    Temporarily disable Tauri for faster Rust development

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,13 @@
-#![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
-)]
+// #![cfg_attr(
+//     all(not(debug_assertions), target_os = "windows"),
+//     windows_subsystem = "windows"
+// )]
+mod axis;
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    // tauri::Builder::default()
+    //     .run(tauri::generate_context!())
+    //     .expect("error while running tauri application");
+
+    println!("Hello, world!");
 }
