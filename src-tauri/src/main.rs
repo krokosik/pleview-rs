eb6fc1a7e7dc2a8d7b8ebd3193f79cebce0ed416
wkrasnicki <wikkra@gmail.com>
2022-05-28 18:59:56 +0200

    Bring back tauri init steps to start writing commands

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,14 @@
-// #![cfg_attr(
-//     all(not(debug_assertions), target_os = "windows"),
-//     windows_subsystem = "windows"
-// )]
+#![allow(dead_code)]
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
 mod axis;
 
 fn main() {
-    // tauri::Builder::default()
-    //     .run(tauri::generate_context!())
-    //     .expect("error while running tauri application");
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 
     println!("Hello, world!");
 }
