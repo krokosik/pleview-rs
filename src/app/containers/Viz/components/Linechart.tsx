801913bdf2b62b07e00681f6a831ee0061893d16
wkrasnicki <wikkra@gmail.com>
2023-04-12 23:45:16 +0200

    Working linechart markers

--- src/app/containers/Viz/components/Linechart.tsx
+++ src/app/containers/Viz/components/Linechart.tsx
@@ -1,6 +1,8 @@
-import { FC, useMemo } from 'react';
+import { FC, useCallback, useMemo, useRef } from 'react';
 import { Data, Layout } from 'plotly.js';
 import Plot from 'react-plotly.js';
+import { select } from 'd3-selection';
+import { D3DragEvent, drag } from 'd3-drag';
 import { merge } from 'lodash';
 import { blueprintLayout } from '../common-layout';
 
@@ -9,10 +11,12 @@ interface LinechartProps {
     yData: number[];
     centralPixel: number;
     onMarkerDrag?: (pixel: number) => void;
-    onMarkerDragEnd?: (pixel: number) => void;
 }
 
-export const Linechart: FC<LinechartProps> = ({ xData, yData, centralPixel, onMarkerDrag, onMarkerDragEnd }) => {
+// TODO: fix marker not working on first render and weird jump at the beginning
+export const Linechart: FC<LinechartProps> = ({ xData, yData, centralPixel, onMarkerDrag }) => {
+    const ref = useRef<HTMLDivElement | null>(null);
+
     const data: Partial<Data> = {
         x: xData,
         y: yData,
@@ -46,8 +50,24 @@ export const Linechart: FC<LinechartProps> = ({ xData, yData, centralPixel, onMa
         [x0, x1],
     );
 
+    const registerDrag = useCallback(() => {
+        const shape = select(ref.current).select('g.layer-above').select('g.shapelayer').select('path');
+        shape.style('pointer-events', 'all').style('cursor', 'ew-resize');
+        let closestIndex = centralPixel;
+        shape.call(
+            // @ts-ignore - d3 typings are terrible, I don't think it's worth fixing
+            drag().on('drag', (e: D3DragEvent<SVGPathElement, never, number>) => {
+                const { x, width } = (select(ref.current).select('g.plot').node() as SVGPathElement).getBoundingClientRect();
+                closestIndex = Math.max(Math.min(Math.round(((e.x - x) / width) * (xData.length - 1)), xData.length - 1), 0);
+                if (closestIndex !== centralPixel) {
+                    onMarkerDrag?.(closestIndex);
+                }
+            }),
+        );
+    }, [centralPixel, onMarkerDrag, xData.length]);
+
     return (
-        <div style={{ height: '100%', width: '100%' }}>
+        <div style={{ height: '100%', width: '100%' }} ref={ref}>
             <Plot
                 data={[data]}
                 layout={layout}
@@ -58,6 +78,7 @@ export const Linechart: FC<LinechartProps> = ({ xData, yData, centralPixel, onMa
                         onMarkerDrag?.(point.pointIndex);
                     }
                 }}
+                onAfterPlot={registerDrag}
             />
             ;
         </div>
